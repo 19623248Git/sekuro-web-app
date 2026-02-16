5925aef2c0e0abc22817d9ddc6c39333d19ec993
19623248Git <13523032@std.stei.itb.ac.id>
2026-02-16 16:27:20 +0700

    feat: :ambulance: FE redirect fix push

--- components/admin/admin_login-form.tsx
+++ components/admin/admin_login-form.tsx
@@ -38,7 +38,9 @@ export function LoginForm({
         password,
       });
       if (error) throw error;
-      // Update this route to redirect to an authenticated route. The user already has an active session.
+      // Refresh the router to update the session
+      router.refresh();
+      // Redirect to admin home page
       router.push("/admin/home");
     } catch (error: unknown) {
       setError(error instanceof Error ? error.message : "An error occurred");
